a6fa5e4501e8419e1865790fe873c2fdb8e758a8
Nazarra <bartekdygon@gmail.com>
2020-07-14 20:07:10 +0200

    header done

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -9,22 +9,30 @@ const HomeHeader = () => {
         <>
             <header className={"homeHeader"}>
                 <div className={"header_Image"}></div>
-                <section className={"top_menu"}>
-                    <div className={"login_and_register_container"}>
-                        <LinkedButton linkTarget={'/logIn'} buttonClassName={"register_and_login_button login"} buttonContent={"Zaloguj"} />
-                        <LinkedButton linkTarget={'/register'} buttonClassName={"register_and_login_button register"} buttonContent={"Załóż konto"} />
-                    </div>
-                        <nav className={"navigation"}>
-                            <ul>
-                               <ScrollLink linkTarger={"..."} LinkName={"Start"} />
-                                <ScrollLink linkTarger={"..."} LinkName={"O co chodzi?"} />
-                                <ScrollLink linkTarger={"..."} LinkName={"o nas"} />
-                                <ScrollLink linkTarger={"..."} LinkName={"Fundacja i organizacje"} />
-                                <ScrollLink linkTarger={"..."} LinkName={"Kontakt"} />
-                            </ul>
-                        </nav>
-                </section>
-
+                    <section className={"Header_Content"}>
+                        <div className={"top_menu"}>
+                            <div className={"login_and_register_container"}>
+                                <LinkedButton linkTarget={'/logIn'} buttonClassName={"register_and_login_button login"} buttonContent={"Zaloguj"} />
+                                <LinkedButton linkTarget={'/register'} buttonClassName={"register_and_login_button register"} buttonContent={"Załóż konto"} />
+                            </div>
+                                <nav className={"navigation"}>
+                                    <ul>
+                                       <ScrollLink linkTarger={"..."} LinkName={"Start"} />
+                                        <ScrollLink linkTarger={"..."} LinkName={"O co chodzi?"} />
+                                        <ScrollLink linkTarger={"..."} LinkName={"o nas"} />
+                                        <ScrollLink linkTarger={"..."} LinkName={"Fundacja i organizacje"} />
+                                        <ScrollLink linkTarger={"..."} LinkName={"Kontakt"} />
+                                    </ul>
+                                </nav>
+                        </div>
+                        <div className={"intro"}>
+                            <h1>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
+                            <div className={"intro_content"}>
+                                <LinkedButton linkTarget={'/logIn'} buttonClassName={"intro_button give"} buttonContent={"ODDAJ RZECZY"} />
+                                <LinkedButton linkTarget={'/logIn'} buttonClassName={"intro_button collection"} buttonContent={"ZORGANIZUJ ZBIÓRKĘ"} />
+                            </div>
+                        </div>
+                     </section>
             </header>
         </>
     );
--- src/components/ScrollLink.js
+++ src/components/ScrollLink.js
@@ -6,12 +6,14 @@ function ScrollLink(props) {
    return(
     <>
         <li>
+            <div>
             <Link to={props.linkTarger}
                   smooth={true}
                   duration={500}
             >
                 <a href={"/"}> {props.LinkName} </a>
             </Link>
+            </div>
         </li>
     </>
 )}
